802475105e79c19c47433fa694ea1b4c673874f0
Gianfranco Diaz <gianfrancodiaz011@gmail.com>
2025-10-20 00:20:05 -0500

    Project init

---
+++ Licitador.WebAPI/Controllers/ApiControllerBase.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Licitador.WebAPI.Controllers;
+
+[ApiController]
+public abstract class ApiControllerBase : ControllerBase
+{
+    
+}
---
+++ Licitador.WebAPI/Controllers/AuthController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Licitador.WebAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    public AuthController()
+    {
+        
+    }
+    
+    
+}
